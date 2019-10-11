ed857cb2052c8ac64780c7977ce5ae43c6ea62c4
liolay <794075795@qq.com>
2019-10-11 13:24:04 +0800

    release

--- src/main/java/cn/ocoop/framework/safe/SessionAutoConfiguration.java
+++ src/main/java/cn/ocoop/framework/safe/SessionAutoConfiguration.java
@@ -3,12 +3,14 @@ package cn.ocoop.framework.safe;
 import cn.ocoop.framework.safe.ann.handler.advice.AuthorizationAttributeSourceAdvisor;
 import cn.ocoop.framework.safe.auth.controller.CaptchaController;
 import cn.ocoop.framework.safe.auth.service.AuthorizingService;
+import cn.ocoop.framework.safe.filter.SafeFilter;
 import cn.ocoop.framework.safe.response.FieldFilterAdvice;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.*;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -55,4 +57,10 @@ public class SessionAutoConfiguration {
         return new CaptchaController();
     }
 
+    @Bean
+    public static FilterRegistrationBean<SafeFilter> safeFilterRegistration() {
+        FilterRegistrationBean<SafeFilter> registration = new FilterRegistrationBean<>(new SafeFilter());
+        registration.setOrder(Integer.MIN_VALUE + 50);
+        return registration;
+    }
 }
